b846b3a183d043480b5515794d2396f243dbcf98
maystrenco <maystrenco@ro.ru>
2022-09-07 16:30:57 +0300

    {uml}

---
+++ src/dotnet/assemblies/BinaryStudio.Modeling.UnifiedModelingLanguage/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Windows.Markup;
+
+#if UseWPF
+[assembly: Guid("0ad08c6f-4900-484b-aaf5-a8542da99d6a")]
+[assembly: XmlnsPrefix("http://schemas.helix.global", "u")]
+[assembly: XmlnsDefinition("http://schemas.helix.global", "BinaryStudio.Modeling.UnifiedModelingLanguage")]
+#endif
