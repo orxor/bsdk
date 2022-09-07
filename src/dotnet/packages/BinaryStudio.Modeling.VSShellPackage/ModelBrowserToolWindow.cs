fcc66f20ffd9f00b458401a29cbeeb453f5261f9
maystrenco <maystrenco@ro.ru>
2022-09-07 17:46:09 +0300

    {modeling-package}

---
+++ src/dotnet/packages/BinaryStudio.Modeling.VSShellPackage/ModelBrowserToolWindow.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+namespace BinaryStudio.Modeling.VSShellPackage
+    {
+    [Guid("57f64175-f95b-4e5a-b588-8c212db72594")]
+    public class ModelBrowserToolWindow : ToolWindowPane
+        {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ModelBrowserToolWindow"/> class.
+        /// </summary>
+        public ModelBrowserToolWindow() : base(null)
+            {
+            Caption = "Model Browser";
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            Content = new ModelBrowserControl();
+            }
+        }
+    }
---
+++ src/dotnet/packages/BinaryStudio.Modeling.VSShellPackage/ModelBrowserToolWindowCommand.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio;
+using Task = System.Threading.Tasks.Task;
+
+namespace BinaryStudio.Modeling.VSShellPackage
+    {
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class ModelBrowserToolWindowCommand
+        {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const Int32 CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("fdc929a3-8dfd-4def-b927-43ab6a8b7f8b");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ModelBrowserToolWindowCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private ModelBrowserToolWindowCommand(AsyncPackage package, OleMenuCommandService commandService)
+            {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+            }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static ModelBrowserToolWindowCommand Instance
+            {
+            get;
+            private set;
+            }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+            {
+            get
+                {
+                return this.package;
+                }
+            }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+            {
+            // Switch to the main thread - the call to AddCommand in ModelBrowserToolWindowCommand's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new ModelBrowserToolWindowCommand(package, commandService);
+            }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(Object sender, EventArgs e) {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            var window = package.FindToolWindow(typeof(ModelBrowserToolWindow), 0, true);
+            if ((null != window) && (null != window.Frame)) {
+                var windowFrame = (IVsWindowFrame)window.Frame;
+                ErrorHandler.ThrowOnFailure(windowFrame.Show());
+                }
+            }
+        }
+    }
