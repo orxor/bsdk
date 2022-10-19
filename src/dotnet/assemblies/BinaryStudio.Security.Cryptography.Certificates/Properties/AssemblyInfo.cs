ce108eb355760c235d4b7117dd2d0eb380d0a027
maystrenco <maystrenco@ro.ru>
2022-10-19 14:05:18 +0300

    {PlatformUISample}

---
+++ src/dotnet/assemblies/BinaryStudio.Security.Cryptography.Certificates/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows.Markup;
+
+[assembly: InternalsVisibleTo("BinaryStudio.Security.Cryptography.CryptographyServiceProvider")]
+[assembly: InternalsVisibleTo("BinaryStudio.Security.Cryptography.CryptographicMessageSyntax")]
+[assembly: InternalsVisibleTo("BinaryStudio.Security.Cryptography.PlatformUI")]
+
+[assembly: XmlnsPrefix("http://schemas.helix.global", "u")]
+[assembly: XmlnsDefinition("http://schemas.helix.global", "BinaryStudio.Security.Cryptography.Certificates")]
