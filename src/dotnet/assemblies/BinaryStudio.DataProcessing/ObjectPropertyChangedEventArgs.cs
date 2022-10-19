967f858d627d1dd74273a5dbdfdb42fedad9eb11
maystrenco <maystrenco@ro.ru>
2022-10-19 11:14:55 +0300

    {HexBox}

---
+++ src/dotnet/assemblies/BinaryStudio.DataProcessing/ObjectPropertyChangedEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+
+namespace BinaryStudio.DataProcessing
+    {
+    public class ObjectPropertyChangedEventArgs<T> : EventArgs
+        {
+        public T OldValue { get; }
+        public T NewValue { get; }
+        public ObjectPropertyChangedEventArgs(T o, T n) {
+            OldValue = o;
+            NewValue = n;
+            }
+
+        public ObjectPropertyChangedEventArgs(DependencyPropertyChangedEventArgs e) {
+            OldValue = (T)e.OldValue;
+            NewValue = (T)e.NewValue;
+            }
+        }
+    }
--- src/dotnet/assemblies/BinaryStudio.PlatformUI/Properties/AssemblyInfo.cs
+++ src/dotnet/assemblies/BinaryStudio.PlatformUI/Properties/AssemblyInfo.cs
@@ -8,6 +8,8 @@ using System.Windows.Markup;
 [assembly: ThemeInfo(ResourceDictionaryLocation.SourceAssembly, ResourceDictionaryLocation.SourceAssembly)]
 [assembly: XmlnsPrefix("http://schemas.helix.global", "u")]
 [assembly: XmlnsDefinition("http://schemas.helix.global", "BinaryStudio.PlatformUI")]
+[assembly: XmlnsDefinition("http://schemas.helix.global", "BinaryStudio.PlatformUI.Controls")]
+[assembly: XmlnsDefinition("http://schemas.helix.global", "BinaryStudio.PlatformUI.Controls.Markups")]
 [assembly: XmlnsDefinition("http://schemas.helix.global", "BinaryStudio.PlatformUI.Controls.Primitives")]
 [assembly: XmlnsDefinition("http://schemas.helix.global", "BinaryStudio.PlatformUI.Models")]
 [assembly: XmlnsDefinition("http://schemas.helix.global", "BinaryStudio.PlatformUI.Media.Effects")]
