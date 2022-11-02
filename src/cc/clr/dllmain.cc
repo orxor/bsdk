c73bfcd693089cd8ca8429783292da01932b6174
maystrenco <maystrenco@ro.ru>
2022-11-02 22:23:41 +0300

    {clr}

--- src/cc/clr/dllmain.cc
+++ src/cc/clr/dllmain.cc
@@ -1,19 +1,16 @@
-// dllmain.cpp : Defines the entry point for the DLL application.
-#include "hdrstop.h"
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
-{
+#include "hdrstop.h"
+#if defined(_WIN32) || defined(_WIN64)
+BOOL APIENTRY DllMain(HMODULE hModule,DWORD ul_reason_for_call,LPVOID) {
     switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
+        {
+        case DLL_PROCESS_ATTACH:
+        case DLL_THREAD_ATTACH:
+        case DLL_THREAD_DETACH:
+        case DLL_PROCESS_DETACH:
+            break;
+        }
     return TRUE;
-}
+    }
+#endif
 
