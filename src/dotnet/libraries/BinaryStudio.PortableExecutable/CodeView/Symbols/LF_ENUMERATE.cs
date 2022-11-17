039e60b57d4e8e83bd5e349e7735b4584947b86a
maystrenco <maystrenco@ro.ru>
2022-11-17 13:38:56 +0300

    {LF_ENUMERATE_ST}

--- src/dotnet/libraries/BinaryStudio.PortableExecutable/CodeView/Symbols/LF_ENUMERATE.cs
+++ src/dotnet/libraries/BinaryStudio.PortableExecutable/CodeView/Symbols/LF_ENUMERATE.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
 using BinaryStudio.PortableExecutable.Win32;
@@ -40,6 +42,43 @@ namespace BinaryStudio.PortableExecutable.CodeView
         public override String ToString()
             {
             return $"{LeafIndex}:{Name}={Value}";
+            }
+
+        private static readonly String[] AccessProtection =
+            {
+            "none",
+            "private",
+            "protected",
+            "public"
+            };
+
+        private static readonly String[] MethodProperties =
+            {
+            "vanilla",
+            "virtual",
+            "static",
+            "friend",
+            "intro",
+            "purevirt",
+            "pureintro"
+            };
+
+        /// <summary>Writes DUMP with specified flags.</summary>
+        /// <param name="Writer">The <see cref="TextWriter"/> to write to.</param>
+        /// <param name="LinePrefix">The line prefix for formatting purposes.</param>
+        /// <param name="Flags">DUMP flags.</param>
+        public override void WriteTo(TextWriter Writer, String LinePrefix, FileDumpFlags Flags) {
+            var flags = new List<String>();
+            if ((Attributes & 0x03) > 0) { flags.Add(AccessProtection[Attributes & 0x03]); }
+            flags.Add(MethodProperties[(Attributes >> 2) & 0x07]);
+            if ((Attributes & 0x0020) == 0x0020) { flags.Add("pseudo");      }
+            if ((Attributes & 0x0040) == 0x0040) { flags.Add("noinherit");   }
+            if ((Attributes & 0x0080) == 0x0080) { flags.Add("noconstruct"); }
+            if ((Attributes & 0x0100) == 0x0100) { flags.Add("compgenx");    }
+            if ((Attributes & 0x0200) == 0x0200) { flags.Add("sealed");      }
+            Writer.WriteLine("{0}LeafIndex:{1} Attributes:[{2}] Value:{3} \"{4}\"",
+                LinePrefix,LeafIndex,String.Join(",",flags),
+                ToString(Value,Flags),Name);
             }
         }
     }
