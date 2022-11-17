57f5594a43b448092cb3d21663f3f4db6f4a9a39
maystrenco <maystrenco@ro.ru>
2022-11-17 19:05:11 +0300

    {shell}

---
+++ src/dotnet/shell/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BinaryStudio.PlatformUI;
+using BMainWindow = BinaryStudio.PlatformUI.MainWindow;
+
+public partial class MainWindow : BMainWindow
+    {
+    public MainWindow()
+        {
+        InitializeComponent();
+        }
+
+    #region M:OnLoaded(Object,RoutedEventArgs)
+    private void OnLoaded(Object sender, RoutedEventArgs e)
+        {
+        Theme.Apply(Theme.Themes[3]);
+        }
+    #endregion
+    }
