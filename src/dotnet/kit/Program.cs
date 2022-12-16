622f590437e3ec0c50f22afea652983edd0942c5
maystrenco <maystrenco@hotmail.com>
2022-12-16 06:39:20 -0500

    {linux}

--- src/dotnet/kit/Program.cs
+++ src/dotnet/kit/Program.cs
@@ -12,14 +12,20 @@ internal class Program
     private static void Main(String[] args) {
         var assembly = Assembly.GetEntryAssembly();
         using (new ColorScope()) {
+            #if !LINUX
             if (PlatformContext.IsParentProcess(Path.GetFileName(assembly.Location))) {
                 FreeConsole();
                 Validate(AttachConsole(-1));
                 }
+            #endif
             Int32 exitcode;
+            #if LINUX
+            using (var client = new LocalClient())
+            #else
             using (var client = PlatformContext.IsRunningUnderServiceControl
                     ? (ILocalClient)(new LocalService())
                     : (ILocalClient)(new LocalClient()))
+            #endif
                 {
                 exitcode = client.Main(args);
                 }
@@ -33,6 +39,7 @@ internal class Program
             }
         }
 
+    #if !LINUX
     [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true, ExactSpelling = true)] private static extern IntPtr LoadLibraryExW([In] string lpwLibFileName, [In] IntPtr hFile, [In] uint dwFlags);
     [DllImport("kernel32.dll", SetLastError = true)] private static extern Boolean FreeLibrary(IntPtr module);
     [DllImport("kernel32.dll", BestFitMapping = false, CharSet = CharSet.Ansi)] private static extern IntPtr GetProcAddress(IntPtr module, String procedure);
@@ -42,4 +49,5 @@ internal class Program
     [DllImport("kernel32.dll")] private static extern Boolean FreeConsole();
     [DllImport("kernel32.dll")] private static extern Boolean AllocConsole();
     [DllImport("ntdll.dll", CharSet = CharSet.Auto)] private static extern unsafe UInt32 NtQueryInformationProcess(IntPtr process, Int32 iclass, void* pi, UInt32 pisz, out UInt32 r);
+    #endif
     }
