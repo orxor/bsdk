e7b06afb31a06e2b698b65d5650727e7df04f93b
maystrenco <maystrenco@ro.ru>
2022-12-27 21:36:31 +0300

    {Win32ErrorCode}

--- src/dotnet/tests/UnitTests.BinaryStudio.PlatformComponents/HResultT.cs
+++ src/dotnet/tests/UnitTests.BinaryStudio.PlatformComponents/HResultT.cs
@@ -11,17 +11,18 @@ namespace UnitTests.BinaryStudio.PlatformComponents
             {
             }
 
-        [TestMethod]
-        public void FormatMessage() {
+        private void FormatMessage<T>()
+            where T: struct, Enum
+            {
             #if NET40 || NET47
-            foreach (var name in Enum.GetNames(typeof(HResult))) {
-                var scode = (Int32)(HResult)Enum.Parse(typeof(HResult),name);
+            foreach (var name in Enum.GetNames(typeof(T))) {
+                var scode = (Int32)(Object)(T)Enum.Parse(typeof(T),name);
                 var value = HResultException.FormatMessage(unchecked((UInt32)scode));
                 Assert.IsNotNull(value);
                 }
             #else
-            foreach (var name in Enum.GetNames<HResult>()) {
-                var scode = (Int32)(HResult)Enum.Parse<HResult>(name);
+            foreach (var name in Enum.GetNames<T>()) {
+                var scode = (Int32)(Object)(T)Enum.Parse<T>(name);
                 var value = HResultException.FormatMessage(unchecked((UInt32)scode));
                 Console.WriteLine($"{name}:{value}");
   //              Console.WriteLine($@"  <data name=""{name}"" xml:space=""preserve"">
@@ -30,6 +31,12 @@ namespace UnitTests.BinaryStudio.PlatformComponents
                 Assert.IsNotNull(value);
                 }
             #endif
+            }
+
+        [TestMethod]
+        public void FormatMessage() {
+            //FormatMessage<HResult>();
+            FormatMessage<Win32ErrorCode>();
             }
         }
     }
