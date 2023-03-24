35fcb6bc1724fa5304c6c92bf7341e837ab912df
Dmitry Maistrenko <maistrenko@fintech.ru>
2023-03-24 16:28:58 +0300

    Update ShapeSheet.cs

--- src/dotnet/libraries/BinaryStudio.DataProcessing/DocumentFormat/OpenXml/Office/Visio/ShapeSheet.cs
+++ src/dotnet/libraries/BinaryStudio.DataProcessing/DocumentFormat/OpenXml/Office/Visio/ShapeSheet.cs
@@ -6,6 +6,8 @@ namespace DocumentFormat.OpenXml.Office.Visio
     {
     public abstract class ShapeSheet : OpenXmlServiceCompositeElement
         {
+        public static Double PointsPerInch = 72.0;
+
         public UInt32? FillStyle { get;set; }
         public UInt32? LineStyle { get;set; }
         public UInt32? TextStyle { get;set; }
@@ -14,6 +16,14 @@ namespace DocumentFormat.OpenXml.Office.Visio
         public Double PinY { get;set; }
         public Double Width  { get;set; }
         public Double Height { get;set; }
+        public Double LineWeight { get;set; }
+        public String LineColor { get;set; }
+
+        protected ShapeSheet()
+            {
+            LineWeight = 1.0;
+            LineColor = "#000000";
+            }
 
         protected override void WriteAttributesTo(XmlWriter writer) {
             writer.WriteAttributeValueIfNotNull("FillStyle",FillStyle);
@@ -31,6 +41,8 @@ namespace DocumentFormat.OpenXml.Office.Visio
             WriteCellTo(writer,nameof(Height),Height);
             WriteCellTo(writer,"LocPinX",Width *0.5);
             WriteCellTo(writer,"LocPinY",Height*0.5);
+            WriteCellTo(writer,nameof(LineWeight),LineWeight/PointsPerInch);
+            WriteCellTo(writer,nameof(LineColor),LineColor);
             Text?.WriteTo(writer);
             }
 
@@ -38,6 +50,13 @@ namespace DocumentFormat.OpenXml.Office.Visio
             writer.WriteStartElement("Cell", NamespaceUri);
             writer.WriteAttributeString("N", N);
             writer.WriteAttributeString("V", V.ToString(US));
+            writer.WriteEndElement();
+            }
+
+        protected void WriteCellTo(XmlWriter writer, String N, String V) {
+            writer.WriteStartElement("Cell", NamespaceUri);
+            writer.WriteAttributeString("N", N);
+            writer.WriteAttributeString("V", V);
             writer.WriteEndElement();
             }
         }
